a734119c963b47d82d9ef64fadaf01bfbd12edfc
Matthew Haney <thewpaney@gmail.com>
2018-02-05 15:47:43 -0700

    Pass PLATFORM macro to C preprocessor that holds value of $(PLATFORM) passed to Make (defaults to HOST). Moved map file from CFLAGS to LDFLAGS. Split up assembly file variables into generated assembly (ASMFILES) and source assembly (SSRC). Only generated assembly files are cleaned up. Also, dependency files are not generated for source assembly files. In `sources.mk`, the SOURCES variable is split into CSRC and SSRC. Also, in Makefile: pass full CFLAGS to dependency generation command, not just INCLUDE_FLAGS. That way the correct PLATFORM variable is set (which is necessary for error-free execution).

--- src/sources.mk
+++ src/sources.mk
@@ -1,4 +1,6 @@
-SOURCES:=conversion.c data.c debug.c main.c memory.c project1.c
+CSRC:=conversion.c data.c debug.c main.c memory.c project1.c
+SSRC:=
 ifeq ($(PLATFORM),KL25Z)
-SOURCES:=$(SOURCES) startup_MKL25Z4.S system_MKL25Z4.c
+CSRC:=$(CSRC) system_MKL25Z4.c
+SSRC:=$(SSRC) startup_MKL25Z4.S
 endif
