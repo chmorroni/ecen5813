0bfee5b4bd00bf9316eff254170b3736124e89d6
Matthew Haney <thewpaney@gmail.com>
2018-02-05 15:45:10 -0700

    Added platform-independent `print_array` function that makes use of PRINTF macro (defined in `platform.h`). Tested compilation for KL25Z, and no warnings or errors were thrown.

--- include/common/platform.h
+++ include/common/platform.h
@@ -8,6 +8,26 @@
 #ifndef __PLATFORM_H__
 #define __PLATFORM_H__
 
+#ifndef PLATFORM
+#error "Please define a PLATFORM variable using the -D option."
+#endif
 
+#if(PLATFORM == KL25Z)
+/* Disable use of printf in KL25Z target executable */
+/* Also suppress warnings through a clever define */
+#define PRINTF (void)
+
+#elif(PLATFORM == BBB)
+/* Enable use of printf in BeagleBone target executable */
+#define PRINTF printf
+
+#elif(PLATFORM == HOST)
+/* Enable use of printf in host target executable */
+#define PRINTF printf
+
+#else
+#error "Unsupported platform."
+
+#endif
 
 #endif /* __PLATFORM_H__ */
--- src/debug.c
+++ src/debug.c
@@ -8,6 +8,16 @@
  * @date 01/31/2018
  */
 
+#include <stdint.h>
+#include "platform.h"
+
 void print_array(uint8_t * start, uint32_t length)
 {
+#ifdef DEBUG
+  uint8_t i;
+  for (i = 0; i < length; i++) {
+    PRINTF("%x", start + i);
+  }
+#endif
+  return;
 }
