e2a634f253af23d2020a27f354783ec22dd41747
Matthew Haney <thewpaney@gmail.com>
2018-02-09 11:07:23 -0700

    Fixed some issues in the Makefile. Made `data.c` work both on BBB and HOST platforms.

--- src/data.c
+++ src/data.c
@@ -25,6 +25,8 @@
 
 void print_cstd_type_sizes()
 {
+#ifdef PLATFORM_HOST
+  /* Unsigned long for host platform */
   PRINTF("char: %lu bytes\n", sizeof(char));
   PRINTF("short: %lu bytes\n", sizeof(short));
   PRINTF("int: %lu bytes\n", sizeof(int));
@@ -37,10 +39,28 @@ void print_cstd_type_sizes()
   PRINTF("signed char: %lu bytes\n", sizeof(signed char));
   PRINTF("signed int: %lu bytes\n", sizeof(signed int));
   PRINTF("signed long: %lu bytes\n", sizeof(signed long));
+#endif
+#ifdef PLATFORM_BBB
+  /* Just unsigned int for BBB */
+  PRINTF("char: %u bytes\n", sizeof(char));
+  PRINTF("short: %u bytes\n", sizeof(short));
+  PRINTF("int: %u bytes\n", sizeof(int));
+  PRINTF("long: %u bytes\n", sizeof(long));
+  PRINTF("double: %u bytes\n", sizeof(double));
+  PRINTF("float: %u bytes\n", sizeof(float));
+  PRINTF("unsigned char: %u bytes\n", sizeof(unsigned char));
+  PRINTF("unsigned int: %u bytes\n", sizeof(unsigned int));
+  PRINTF("unsigned long: %u bytes\n", sizeof(unsigned long));
+  PRINTF("signed char: %u bytes\n", sizeof(signed char));
+  PRINTF("signed int: %u bytes\n", sizeof(signed int));
+  PRINTF("signed long: %u bytes\n", sizeof(signed long));
+#endif
 }
 
 void print_stdint_type_sizes()
 {
+#ifdef PLATFORM_HOST
+  /* Unsigned long for host platform */
   PRINTF("int8_t: %lu bytes\n", sizeof(int8_t));
   PRINTF("uint8_t: %lu bytes\n", sizeof(uint8_t));
   PRINTF("int16_t: %lu bytes\n", sizeof(int16_t));
@@ -55,10 +75,30 @@ void print_stdint_type_sizes()
   PRINTF("uint_least32_t: %lu bytes\n", sizeof(uint_least32_t));
   PRINTF("size_t: %lu bytes\n", sizeof(size_t));
   PRINTF("ptrdiff_t: %lu bytes\n", sizeof(ptrdiff_t));
+#endif
+#ifdef PLATFORM_BBB
+  /* Just unsigned int for BBB */
+  PRINTF("int8_t: %u bytes\n", sizeof(int8_t));
+  PRINTF("uint8_t: %u bytes\n", sizeof(uint8_t));
+  PRINTF("int16_t: %u bytes\n", sizeof(int16_t));
+  PRINTF("uint16_t: %u bytes\n", sizeof(uint16_t));
+  PRINTF("int32_t: %u bytes\n", sizeof(int32_t));
+  PRINTF("uint32_t: %u bytes\n", sizeof(uint32_t));
+  PRINTF("uint_fast8_t: %u bytes\n", sizeof(uint_fast8_t));
+  PRINTF("uint_fast16_t: %u bytes\n", sizeof(uint_fast16_t));
+  PRINTF("uint_fast32_t: %u bytes\n", sizeof(uint_fast32_t));
+  PRINTF("uint_least8_t: %u bytes\n", sizeof(uint_least8_t));
+  PRINTF("uint_least16_t: %u bytes\n", sizeof(uint_least16_t));
+  PRINTF("uint_least32_t: %u bytes\n", sizeof(uint_least32_t));
+  PRINTF("size_t: %u bytes\n", sizeof(size_t));
+  PRINTF("ptrdiff_t: %u bytes\n", sizeof(ptrdiff_t));
+#endif
 }
 
 void print_pointer_sizes()
 {
+#ifdef PLATFORM_HOST
+  /* Unsigned long for host platform */
   PRINTF("char*: %lu bytes\n", sizeof(char*));
   PRINTF("short*: %lu bytes\n", sizeof(short*));
   PRINTF("int*: %lu bytes\n", sizeof(int*));
@@ -72,6 +112,23 @@ void print_pointer_sizes()
   PRINTF("char**: %lu bytes\n", sizeof(char**));
   PRINTF("int**: %lu bytes\n", sizeof(int**));
   PRINTF("void**: %lu bytes\n", sizeof(void**));
+#endif
+#ifdef PLATFORM_BBB
+  /* Just unsigned int for BBB */
+  PRINTF("char*: %u bytes\n", sizeof(char*));
+  PRINTF("short*: %u bytes\n", sizeof(short*));
+  PRINTF("int*: %u bytes\n", sizeof(int*));
+  PRINTF("long*: %u bytes\n", sizeof(long*));
+  PRINTF("double*: %u bytes\n", sizeof(double*));
+  PRINTF("float*: %u bytes\n", sizeof(float*));
+  PRINTF("void*: %u bytes\n", sizeof(void*));
+  PRINTF("int8_t*: %u bytes\n", sizeof(int8_t*));
+  PRINTF("int16_t*: %u bytes\n", sizeof(int16_t*));
+  PRINTF("int32_t*: %u bytes\n", sizeof(int32_t*));
+  PRINTF("char**: %u bytes\n", sizeof(char**));
+  PRINTF("int**: %u bytes\n", sizeof(int**));
+  PRINTF("void**: %u bytes\n", sizeof(void**));
+#endif
 }
 
 int32_t swap_data_endianness(uint8_t * data, size_t type_length)
