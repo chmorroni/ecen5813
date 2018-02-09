4aa9356a3da91aab3bf2e67f0c69c992a28569e8
Christopher Morroni <chmorroni@gmail.com>
2018-02-08 22:13:05 -0700

    completed conversion functions

--- src/conversion.c
+++ src/conversion.c
@@ -23,13 +23,45 @@
 #include <stdint.h>
 #include <stddef.h> /* for NULL */
 #include "conversion.h"
+#include "memory.h"
 
 uint8_t my_itoa(int32_t data, uint8_t * ptr, uint32_t base)
 {
   if (ptr == NULL || base < 2 || base > 16) {
     return 0;
   }
-  return 0;
+
+  uint8_t len = 0, val;
+
+  /* check for sign */
+  if(data < 0)
+  {
+    data *= -1;
+    *ptr = '-';
+    len++;
+  }
+
+  /* this uses modulus and divide to find the value in each place */
+  do
+  {
+    val = data % base;
+    *(ptr + len++) = val < 10 ? '0' + val : 'A' + val - 10;
+    data /= base;
+  } while(data != 0);
+
+  /* reverse the numbers */
+  if(*ptr == '-')
+  {
+    my_reverse(ptr + 1, len - 1);
+  }
+  else
+  {
+    my_reverse(ptr, len);
+  }
+
+  *(ptr + len++) = '\0';
+
+  return len;
 }
 
 uint32_t my_atoi(uint8_t * ptr, uint8_t digits, uint32_t base)
@@ -37,5 +69,24 @@ uint32_t my_atoi(uint8_t * ptr, uint8_t digits, uint32_t base)
   if (ptr == NULL || base < 2 || base > 16) {
     return 0;
   }
-  return 0;
+
+  uint32_t val = 0;
+
+  /* start at the second character if negative */
+  uint8_t i = *ptr == '-' ? 1 : 0;
+
+  /* this multiplies and adds to convert to an integer */
+  for(; i < digits - 1; i++)
+  {
+    val *= base;
+    val = (*(ptr + i) > '9') ? (val + *(ptr + i) - 'A' + 10) : (val + *(ptr + i) - '0');
+  }
+
+  /* get the sign */
+  if(*ptr == '-')
+  {
+    val *= -1;
+  }
+
+  return val;
 }
