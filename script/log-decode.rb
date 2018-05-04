5925ac27bea2a204e83493d8a3a5448aaca603b0
Matthew Haney <thewpaney@gmail.com>
2018-05-04 09:46:50 -0600

    Added project 4 demo task

--- script/log-decode.rb
+++ script/log-decode.rb
@@ -4,6 +4,8 @@ verbose = false
 
 require 'zlib'
 
+output_data = ""
+
 log_ids = ["SYSTEM_ID", "SYSTEM_VERSION", "LOGGER_INITIALIZED", "GPIO_INITIALIZED", "SYSTEM_INITIALIZED", "SYSTEM_HALTED", "INFO", "WARNING", "ERROR", "PROFILING_STARTED", "PROFILING_RESULT", "PROFILING_COMPLETED", "DATA_RECEIVED", "DATA_ANALYSIS_STARTED", "DATA_ALPHA_COUNT", "DATA_NUMERIC_COUNT", "DATA_PUNCTUATION_COUNT", "DATA_MISC_COUNT", "DATA_ANALYSIS_COMPLETED", "HEARTBEAT", "CORE_DUMP"]
 
 def SimpleXorCRC(str)
@@ -34,11 +36,11 @@ ARGV.each do |filename|
     log_data += ts_str
     #    lowercase b = LSB first; B for MSB
     timestamp = ts_str.unpack("b8b8b8b8").join.to_i(2)
-    puts "  timestamp: #{timestamp}" if verbose
+    output_data << "  timestamp: #{timestamp}" if verbose
     #  uint8_t id
     log_id = log_array.shift
     log_data += log_id
-    puts "  log ID: #{log_id.unpack("C*").first - 20} (#{log_ids[log_id.unpack("C*").first - 20]})" if verbose
+    output_data << "\n  log ID: #{log_id.unpack("C*").first - 20} (#{log_ids[log_id.unpack("C*").first - 20]})" if verbose
     #  uint8_t source_id
     source_id = log_array.shift
     log_data += source_id
@@ -46,23 +48,25 @@ ARGV.each do |filename|
     source = "BBB" if source_id.unpack("C*").first == 1
     source = "KL25Z" if source_id.unpack("C*").first == 16
     source = "HOST" if source_id.unpack("C*").first == 0
-    puts "  source: #{source}" if verbose
+    output_data << "\n  source: #{source}" if verbose
     #  uint8_t len
     log_data_len = log_array.shift
     log_data += log_data_len
     log_data_len = log_data_len.unpack("C*").first
-    puts "  payload length: #{log_data_len}" if verbose
+    output_data << "\n  payload length: #{log_data_len}" if verbose
     #  uint8_t * payload
     log_payload = log_array.shift(log_data_len).join
     log_data += log_payload
-    puts "  payload: #{log_payload}" if verbose
+    output_data << "\n  payload: #{log_payload}" if verbose
     #  uint32_t crc
     crc = log_array.shift.unpack("C*").first
-    puts "  crc: #{SimpleXorCRC(log_data)} (given: #{crc})" if verbose
+    output_data << "\n  crc: #{SimpleXorCRC(log_data)} (given: #{crc})" if verbose
     unless crc == SimpleXorCRC(log_data)
-      puts "[MALFORMED]"
+      output_data << "\n[MALFORMED]"
     else
-      puts "#{source} - [#{timestamp} : #{log_ids[log_id.unpack("C*").first - 20]}] #{log_payload}"
+      output_data << "#{source} - [#{timestamp} : #{log_ids[log_id.unpack("C*").first - 20]}] #{log_payload}\n"
     end
   end
+  puts output_data
+  File.write("output.log", output_data)
 end
